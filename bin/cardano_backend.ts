ccb3a7bfd95509d107f22a654813314e2b1f62bc
Martin Cornejo <mart256@gmail.com>
2024-03-28 21:37:54 +0100

    Bootstraped correctly

--- bin/cardano_backend.ts
+++ bin/cardano_backend.ts
@@ -3,4 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import { CardanoBackendStack } from '../lib/cardano_backend-stack';
 
 const app = new cdk.App();
-new CardanoBackendStack(app, 'CardanoBackendStack');
+new CardanoBackendStack(app, 'CardanoBackendStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: 'eu-central-1'
+}});

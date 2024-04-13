607b378bf2d3b3a76749db5bcf9dd7f9e0005da7
Martin Cornejo <mart256@gmail.com>
2024-04-13 15:45:47 +0200

    Added DynamoDB table to the infra

--- lambda/src/main/java/com/walruscode/cardano/repositories/WalletRepository.java
+++ lambda/src/main/java/com/walruscode/cardano/repositories/WalletRepository.java
@@ -9,24 +9,20 @@ import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-import java.net.URI;
 import java.time.Instant;
 import java.util.Optional;
 
 public class WalletRepository implements AutoCloseable {
 
-    private final String TABLE_NAME = "wallet";
+    private final String TABLE_NAME = System.getenv("TABLE_NAME");
     private final DynamoDbClient client;
     private final DynamoDbEnhancedClient dbClient;
     private final DynamoDbTable<Wallet> table;
 
     public WalletRepository() {
-        String dynamoDbUrl = System.getenv("DYNAMO_URL");
-
         client = DynamoDbClient
                 .builder()
                 .region(Region.EU_CENTRAL_1)
-                .endpointOverride(URI.create(dynamoDbUrl))
                 .build();
 
         dbClient = DynamoDbEnhancedClient.builder()
--- lib/cardano_backend-stack.ts
+++ lib/cardano_backend-stack.ts
@@ -2,6 +2,7 @@ import {Duration, Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import {HttpLambdaIntegration} from 'aws-cdk-lib/aws-apigatewayv2-integrations'
 import {PayloadFormatVersion} from "aws-cdk-lib/aws-apigatewayv2";
 
@@ -9,12 +10,35 @@ export class CardanoBackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const table = new dynamodb.Table(this, 'wallet', {
+      readCapacity: 1,
+      writeCapacity: 1,
+      partitionKey: {
+        name: 'stake_address',
+        type: dynamodb.AttributeType.STRING
+      }
+    });
+
     const lambda_validation = new lambda.Function(this, 'lambdaValidationHandler', {
       runtime: lambda.Runtime.JAVA_21,
       code: lambda.Code.fromAsset("./lambda/target/app.jar"),
       handler: "com.walruscode.cardano.Router::validateSign",
       memorySize: 1024,
-      timeout: Duration.seconds(10)
+      timeout: Duration.seconds(10),
+      environment: {
+        TABLE_NAME: table.tableName
+      }
+    });
+
+    const lambda_login = new lambda.Function(this, 'lambdaLoginHandler', {
+      runtime: lambda.Runtime.JAVA_21,
+      code: lambda.Code.fromAsset("./lambda/target/app.jar"),
+      handler: "com.walruscode.cardano.Router::login",
+      memorySize: 1024,
+      timeout: Duration.seconds(10),
+      environment: {
+        TABLE_NAME: table.tableName
+      }
     });
 
     const lambda_show_content = new lambda.Function(this, 'lambdaContentHandler', {
@@ -37,18 +61,33 @@ export class CardanoBackendStack extends Stack {
       'LambdaShowContentIntegration',
       lambda_show_content,
       { payloadFormatVersion: PayloadFormatVersion.VERSION_2_0}
-  );
+    );
+
+    const lambdaLoginIntegration = new HttpLambdaIntegration(
+      'LambdaLoginIntegration',
+      lambda_login,
+      { payloadFormatVersion: PayloadFormatVersion.VERSION_2_0}
+    );
 
     httpApi.addRoutes({
-      path: '/coins',
-      methods: [ apigatewayv2.HttpMethod.GET ],
+      path: '/login',
+      methods: [ apigatewayv2.HttpMethod.POST ],
+      integration: lambdaLoginIntegration,
+    });
+
+    httpApi.addRoutes({
+      path: '/validate',
+      methods: [ apigatewayv2.HttpMethod.POST ],
       integration: lambdaValidationIntegration,
     });
 
     httpApi.addRoutes({
-      path: '/show',
+      path: '/home',
       methods: [ apigatewayv2.HttpMethod.GET ],
       integration: lambdaShowContentIntegration,
     });
+
+    table.grantReadData(lambda_validation);
+    table.grantWriteData(lambda_login);
   }
 }
